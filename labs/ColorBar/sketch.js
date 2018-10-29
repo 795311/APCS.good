aff404218c911ad232dfb27222afc599809879ee
795311 <795311@seq.org>
2018-10-29 08:48:57 -0700

    Added Color Bar

---
+++ labs/ColorBar/sketch.js
@@ -0,0 +1,7 @@
+var colorBar = []; //colorbar array
+
+function setup(){ //setup code for project
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(0,0,0); //draw canvas
+}
--- labs/Lab1022 Word Sort/sketch.js
+++ labs/Lab1022 Word Sort/sketch.js
@@ -1,9 +1,27 @@
-
+var txt = [];
+function preload(){
+  txt = loadStrings("words.txt")
+}
 function setup(){
   noCanvas();
+  console.log(txt);
+  bubbleSort(txt);
   txt.push = loadStrings("words.txt");
 }
 
 function draw(){
- bubbleSort(txt);
+}
+
+function bubbleSort(txt){
+  var length = txt.length;
+  for(var i = 0; i < length; i++0){
+    for(var p = 0; p < (length - i - 1); p++){
+      if(txt[p] > txt[p+1]){
+        var temp = txt[p];
+        txt[p] = txt[p+1];
+        txt[p+1] = temp;
+        console.log(txt);
+      }
+    }
+  }
 }
