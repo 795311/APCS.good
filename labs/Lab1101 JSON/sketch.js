793ea84f07d3978d64f91d3015e7ba91a6cf4b0f
795311 <795311@seq.org>
2018-11-05 08:49:50 -0800

    changed JSON file

--- labs/Lab1101 JSON/sketch.js
+++ labs/Lab1101 JSON/sketch.js
@@ -1,9 +1,23 @@
 var data;
 var barHeight;
+var txt;
 
 function preload(){
-  data = loadJSON("Battle4Online.json");
+  txt = loadJSON("population.json");
 }
 function setup (){
-  console.log(data.hooks);
+  console.log(data.countrydata);
+}
+function bubbleSort(txt){
+  var length = txt.length;
+  for(var i = txt- 1; i > txt; i--){
+    for(var p = 0; p < (length - i - 1); p++){
+      if(txt[p] > txt[p+1]){
+        var temp = txt[p];
+        txt[p] = txt[p+1];
+        txt[p+1] = temp;
+        console.log(txt);
+      }
+    }
+  }
 }
