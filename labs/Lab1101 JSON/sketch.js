65f3377a937222a63405bc79445660a1193aae75
795311 <795311@seq.org>
2018-11-06 08:47:56 -0800

    fixed bubble sort

--- labs/Lab1101 JSON/sketch.js
+++ labs/Lab1101 JSON/sketch.js
@@ -1,22 +1,32 @@
 var data;
 var barHeight;
-var txt;
+
 
 function preload(){
-  txt = loadJSON("population.json");
+  data = loadJSON("population.json");
 }
+
 function setup (){
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(20,20,20);
+  bubbleSort();
   console.log(data.countrydata);
 }
-function bubbleSort(txt){
-  var length = txt.length;
-  for(var i = txt- 1; i > txt; i--){
-    for(var p = 0; p < (length - i - 1); p++){
-      if(txt[p] > txt[p+1]){
-        var temp = txt[p];
-        txt[p] = txt[p+1];
-        txt[p+1] = temp;
-        console.log(txt);
+
+function draw(){
+
+}
+function bubbleSort(){
+  var length = data.length;
+  for(var i = length- 1; i > 0; i--){
+    for(var p = 0; p <  i; p++){
+
+      if(data[p] > data[p+1]){
+        var temp = data[p];
+        
+        data[p] = data[p+1];
+        data[p+1] = temp;
       }
     }
   }
