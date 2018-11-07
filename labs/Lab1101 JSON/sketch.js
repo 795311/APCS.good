a6382bf04cfdc77e9f89d9644ff264b98e98348f
795311 <795311@seq.org>
2018-11-07 08:50:44 -0800

    worked

--- labs/Lab1101 JSON/sketch.js
+++ labs/Lab1101 JSON/sketch.js
@@ -20,13 +20,11 @@ function draw(){
 function bubbleSort(){
   var length = data.length;
   for(var i = length- 1; i > 0; i--){
-    for(var p = 0; p <  i; p++){
-
-      if(data[p] > data[p+1]){
-        var temp = data[p];
-        
-        data[p] = data[p+1];
-        data[p+1] = temp;
+    for(var p s= 0; p <  i; p++){
+      if(data.countrydata.males[i] > data.countrydata.male[i+1]){
+        var temp = data.countrydata.males[p];
+        data.countrydata[p] = data.countrydata[p+1];
+        data.countrydata[p+1] = temp;
       }
     }
   }
