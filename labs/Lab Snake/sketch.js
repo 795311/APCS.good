63b10140d3a47bc6d79f6c502946ba62e3bd7d03
795311 <795311@seq.org>
2018-11-26 08:49:07 -0800

    made it move by colm and row

--- labs/Lab Snake/sketch.js
+++ labs/Lab Snake/sketch.js
@@ -4,33 +4,34 @@ var colo, rows;
 var food;
 
 function setup(){
+  frameRate(9);
   var cnv = createCanvas(800, 800);
   var colo = width/w;
   var rows = height/w;
   cnv.position((windowWidth-width)/2, 30);
-  background(0);
   snake = new Snake(createVector(400, 400), createVector(1));
   food = new Food(createVector(random(0,400), color(0,0,255)));
 }
 
 
 function draw(){
+  background(0);
   snake.run();
-  food.run();
   //if snake gets food, startNewRound
 }
 function keyPressed(){
+  text(keyCode, 0, 0);
   if(keyCode === 87){
-    snake.vel = createVector(0, -1);
+    snake.vel = createVector(0, -1*w);
   }
   if(keyCode === 83){
-    snake.vel = createVector(0, 1);
+    snake.vel = createVector(0, 1*w);
   }
   if(keyCode === 68){
-    snake.vel = createVector(1,0);
+    snake.vel = createVector(1*w,0);
   }
   if(keyCode === 65){
-    snake.vel = createVector(-1,0);
+    snake.vel = createVector(-1*w,0);
   }
 }
 function startNewRound(){
